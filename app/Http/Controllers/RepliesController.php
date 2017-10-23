edd1fe7ad6cd3d898426425b192efcaa5e01603e
Andy Tang <kiwikefei@gmail.com>
2017-10-23 20:30:20 +1300

    wipe up reply in memory rather than presist it

--- app/Http/Controllers/RepliesController.php
+++ app/Http/Controllers/RepliesController.php
@@ -8,6 +8,14 @@ use Illuminate\Http\Request;
 class RepliesController extends Controller
 {
     //
+    /**
+     * RepliesController constructor.
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function store(Thread $thread)
     {
         $thread->addReply([
--- tests/Feature/ParticipateInForumTest.php
+++ tests/Feature/ParticipateInForumTest.php
@@ -8,6 +8,17 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 class ParticipateInForumTest extends TestCase
 {
     use RefreshDatabase;
+    
+    /** @test **/
+    public function unauthenticated_users_may_not_add_replies()
+    {
+        $this->expectException('Illuminate\Auth\AuthenticationException');
+//        $thread = factory('App\Thread')->create();
+//        $reply = factory('App\Reply')->create();
+//        $this->post($thread->path() . '/replies', $reply->toArray());
+        $this->post('/threads/1/replies', []);
+    }
+    
     /** @test **/
     public function an_authenticated_user_may_participate_in_forum_threads()
     {
@@ -15,17 +26,20 @@ class ParticipateInForumTest extends TestCase
         // Given: we have an authenticated user
         $this->be(factory('App\User')->create());
 
-        //        And an existing thread
+        // Given: And an existing thread
         $thread = factory('App\Thread')->create();
 
         // When: the user adds a reply to the thread
-        $reply = factory('App\Reply')->create();
+//        $reply = factory('App\Reply')->create();  // rather than create() a reply we , just make() it in memory
+        $reply = factory('App\Reply')->make();
+
         $this->post($thread->path() . '/replies', $reply->toArray());
 
         // Then: their reply should be visible on the page.
+
+        // Action
         $this->get($thread->path())
             ->assertSee($reply->body);
-        // Action
 
     }
 }
