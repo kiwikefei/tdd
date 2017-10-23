b364bb378efd520148aef7fba50bc642adb379eb
Andy Tang <kiwikefei@gmail.com>
2017-10-23 20:46:10 +1300

    user must sign to participate

--- resources/views/threads/reply.blade.php
+++ resources/views/threads/reply.blade.php
@@ -1,8 +1,6 @@
 <div class="panel panel-default">
     <div class="panel-heading">
-        <a href="#">
-            {{ $reply->owner->name }}
-        </a>
+        <a href="#">{{ $reply->owner->name }}</a>
         said
         {{ $reply->created_at->diffForHumans()}}
     </div>
